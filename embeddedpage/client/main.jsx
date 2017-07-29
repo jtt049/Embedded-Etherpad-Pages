571f70c430f3f9714c4d68ddfdb77628704c8d90
Justin <jtt049@ucsd.edu>
2017-07-29 15:30:14 -0700

    make it better

--- embeddedpage/client/main.jsx
+++ embeddedpage/client/main.jsx
@@ -2,11 +2,10 @@ import React, {Component} from 'react';
 import { render  } from 'react-dom';
 import { Meteor } from 'meteor/meteor'
 import App from '../imports/client/App';
+import Iframe from 'react-iframe';
+import ReactTimeout from 'react-timeout';
 
 
-
-if(Meteor.isClient){
 	Meteor.startup(function(){
-		ReactDOM.render(<App />, document.getElementById("render-target"));
+		render(<App />, document.getElementById("render-target"));
 	});
-}
--- embeddedpage/imports/client/App.jsx
+++ embeddedpage/imports/client/App.jsx
@@ -1,11 +1,44 @@
 import React, {Component} from 'react';
+import Iframe from 'react-iframe';
+import ReactTimeout from 'react-timeout';
 
-export default class App extends React.Component{
+
+
+export default class App extends Component{
+	state={
+		on: false
+		}
+	toggle =() => {
+			this.setState({on:!this.state.on})
+		}
+
+
+	handleClick=(e) =>{
+	this.props.setTimeout(this.toggle, 5000)
+}
+	
+
 	render(){
 		return(
 			<div>
-			<h1>directions go here</h1>
+			<p>Hello world, where is button?</p> 
+			<div style={{backgroundColor: (this.state.on ? 'yellow': 'gray')}}>
+			<button onClick={this.handleClick}>Click me! </button>
 			</div>
-			)
+
+			<Iframe url="https://beta.etherpad.org/p/Test1?showControls=true&showChat=true&showLineNumbers=true&useMonospaceFont=false"  width="1280px" height="720px" display="initial" position="relative" allowFullScreen/>
+			
+			</div>
+		
+			);
 	}
 }
+
+/*	class Heading extends Component{
+		render(){
+			return (
+				<h1>{this.props.count}</h1>
+				)
+		}
+	}
+*/	
