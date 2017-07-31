e9c924213574e644122a5e346b51b928341bc810
Justin <jtt049@ucsd.edu>
2017-07-31 12:03:20 -0700

    make it better

--- embeddedpage/imports/client/App.jsx
+++ embeddedpage/imports/client/App.jsx
@@ -5,20 +5,31 @@ import ReactCountdownClock from 'react-countdown-clock';
 
 
 export default class App extends Component{
+	constructor(){
+		super()
+  	    this.state= {
+  	    	isButtonOn : true
+  	    };
+	}
 
+
+	showButton(){
+		this.setState( {isButtonOn: false});
+
+	}
 	
 	render(){
 		return(
 			<div id="whole">
 
 			<div id="counter">
-				<ReactCountdownClock  seconds={480} color="#000" alpha={1.0} size={100} />
+				<ReactCountdownClock  seconds={60} color="#000" alpha={1.0} size={100} onComplete={this.showButton} />
 			</div>
 			<div id="paragraph">
 				<p>Insert Pararaph here</p> 
 			</div>
 			<div id="next">
-				<button> Click me! </button>
+				<button disabled={this.state.isButtonOn}> Next >> </button>
 			</div>
 			<div id="frame">
 				<Iframe url="https://beta.etherpad.org/p/Test1?showControls=true&showChat=true&showLineNumbers=true&useMonospaceFont=false"  width="1280px" height="720px" display="initial" position="relative" allowFullScreen/>
